9a6dbe4ae7e37ccfa570417fedbeb38dd0ddbab6
gili-gili <3409571710@qq.com>
2022-07-12 16:41:01 +0800

    initial submit
    Script ver 1.0.0
    EXE ver 1.1.1

---
+++ cFosSpeed/Start.bat
@@ -0,0 +1,17 @@
+@echo off
+set /a COUNT=1
+echo =============================================
+echo cFosSpeed Traffic Shaping Calibration Script 
+echo Powered by Speedtest, written by gili-gili
+echo https://github.com/gili-gili/Some-Useful-Scripts/cFosSpeed/
+echo =============================================
+set /p TARGET=How many rounds? 
+:run
+echo Round #%COUNT%, running
+start /min cmd /C speedtest
+timeout /nobreak /t 8
+if %COUNT% equ %TARGET% exit
+set /a COUNT=%COUNT%+1
+goto run
+echo Done!
+pause
